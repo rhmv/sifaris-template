e27cd7ee20b0fbe1f1fbd96da45377044d344a8c
rhmv <87086212+rhmv@users.noreply.github.com>
2021-10-05 00:30:35 +0400

    Add files via upload

---
+++ order-js/swiper.js
@@ -0,0 +1,18 @@
+// My Swiper 1
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
